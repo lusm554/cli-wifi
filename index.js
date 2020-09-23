13fe594d0d98db02a994b6de36f726c7566d080d
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-23 15:40:11 +0300

    soft tab

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const { spawn } = require('child_process')
 const getWiFiName = spawn('networksetup', ['-getairportnetwork', 'en0'])
 
 getWiFiName.stdout.on('data', (data) => {
-    // console.log(`stdout: ${data}`);
-    console.log(data.toString())
+  // console.log(`stdout: ${data}`);
+  console.log(data.toString())
 })
 
 getWiFiName.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
+  console.log(`stderr: ${data}`);
 })
 
 getWiFiName.on('error', (error) => {
-    console.error(error);
+  console.error(error);
 })
 
 getWiFiName.on("close", code => {
-    console.log(`child process exited with code ${code}`);
+  console.log(`child process exited with code ${code}`);
 });
